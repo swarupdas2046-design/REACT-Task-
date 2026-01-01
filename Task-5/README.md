18dc7800c43d9b826208997007e0870737eed7a8
Swarup Das <swarupdas2046@gmail.com>
2026-01-01 23:58:25 +0530

    date:01-01-2026

---
+++ Task-5/README.md
@@ -0,0 +1,28 @@
+# 📸 Image Gallery App
+
+A responsive image gallery built using React and Tailwind CSS.  
+Images are fetched from a public API and displayed in a clean grid layout.
+
+## 🚀 Features
+- API data fetching using Axios
+- Fully responsive (mobile, tablet, desktop)
+- Grid-based image layout
+- Clean and minimal UI
+
+## 🛠️ Tech Stack
+- React
+- Tailwind CSS
+- Axios
+- Picsum Photos API
+
+## 📱 Responsive Layout
+- Mobile: 2 columns
+- Tablet: 3–4 columns
+- Desktop: 5 columns
+
+## 🧠 Learnings
+- API handling with Axios
+- React hooks (useState)
+- Responsive design using Tailwind breakpoints
+- Grid vs Flexbox
+
---
+++ Task-5/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import axios from "axios";
+const App = () => {
+  const [User, setUser] = useState([]);
+  let getData = async () => {
+    let Response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=150"
+    );
+    console.log(Response.data[1]);
+    setUser(Response.data);
+  };
+
+  return (
+    // <div className="h-full w-full bg-black flex items-center flex-col p-8 gap-4 ">
+    <div className="w-full max-w-full bg-black border-2 border-amber-200  p-4 overflow-auto">
+    <button
+        onClick={getData}
+        className="px-10 py-5 rounded-2xl bg-pink-800 active:scale-95 text-white font-semibold"
+      >
+        Click Me
+      </button>
+
+      {/* <div className="w-300 h-145 bg-black border-2 border-amber-200 rounded-2xl p-4 flex flex-wrap  gap-3.5 overflow-auto"> */}
+          <div className="grid 
+    grid-cols-2 
+    sm:grid-cols-3 
+    md:grid-cols-4 
+    lg:grid-cols-5 
+    gap-4">
+        {User.map((elem, idx) => {
+          return (
+            <div
+              key={idx}
+              className=" border aspect-square rounded overflow-hidden border-amber-50"
+            >
+              <img
+                className=" h-full w-full object-cover rounded "
+                src={elem.download_url}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
