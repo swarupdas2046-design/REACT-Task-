a499c5e7bd27ca326f2f05cf1079fa211d0fa217
Swarup Das <swarupdas2046@gmail.com>
2026-01-02 00:06:24 +0530

    Some Ui Changes

--- Task-5/src/App.jsx
+++ Task-5/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <div className="w-full max-w-full bg-black border-2 border-amber-200  p-4 overflow-auto">
     <button
         onClick={getData}
-        className="px-10 py-5 rounded-2xl bg-pink-800 active:scale-95 text-white font-semibold"
+        className="px-10 py-5 rounded-2xl bg-pink-800 active:scale-95 mb-4 text-white font-semibold"
       >
         Click Me
       </button>
