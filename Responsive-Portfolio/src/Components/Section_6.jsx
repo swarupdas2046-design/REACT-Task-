49b94f5ee283d9eb17faf1fb2087ef68297a9fa0
Swarup Das <swarupdas2046@gmail.com>
2026-01-16 02:41:22 +0530

    Add Form handling

--- Responsive-Portfolio/src/Components/Section_6.jsx
+++ Responsive-Portfolio/src/Components/Section_6.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Section_6 = () => {
+  const [username, setusername] = useState("");
+  const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
+  const [message, setmessage] = useState("");
+  let allsubmit = (e) => {
+    e.preventDefault();
+    alert("Submitted");
+    setusername("");
+    setphone("");
+    setemail("");
+    setmessage("");
+  };
   return (
     <div id="contact" className=" w-full  text-white  mt-6">
       <h2 className="text-3xl lg:text-4xl text-center font-semibold bg-linear-to-r from-purple-700 to-white bg-clip-text text-transparent  mb-3">
@@ -30,28 +42,47 @@ const Section_6 = () => {
           <p className="text-2xl">Send Message</p>
           <form
             onSubmit={(e) => {
-              e.preventDefault();
-              alert("Submitted");
+              allsubmit(e);
             }}
             className="bg- flex flex-col gap-4"
           >
             <input
+              onChange={(e) => {
+                setusername(e.target.value);
+              }}
+              value={username}
               className="border-2 border-gray-700 bg-black px-5 py-3 rounded-xl "
+              required
               type="text"
               placeholder="Enter name"
             />
             <input
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+              value={email}
               className="border-2 border-gray-700 px-5 py-3 rounded-xl bg-black "
+              required
               type="email"
               placeholder="Enter email"
             />
             <input
+              onChange={(e) => {
+                setphone(e.target.value);
+              }}
+              value={phone}
               className="border-2 border-gray-700 px-5 py-3 rounded-xl bg-black "
+              required
               type="number"
               placeholder="Enter number"
             />
             <input
+              onChange={(e) => {
+                setmessage(e.target.value);
+              }}
+              value={message}
               className="border-2 border-gray-700 px-5 py-3 rounded-xl bg-black "
+              required
               type="text"
               placeholder="Enter message"
             />
